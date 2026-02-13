35253c583d4e80623b4ad9cdf2f5caba559d85b0
zhangxuan2011 <zx20110412@outlook.com>
2026-02-13 13:08:00 +0800

    feat(fs): 添加了mkdir和mkfile功能

--- fs/src/definition.rs
+++ fs/src/definition.rs
@@ -15,10 +15,13 @@ pub struct SuperBlock {
     pub data_start_block: u32,
 
     /// The bitmap which indicates whether each block is used.
-    pub block_bitmap: [u8; 512],    // 512 * 8 = 4096 = 1 block
+    pub block_bitmap: [u8; 128],    // 128 * 8 = 1024 = 1 block
+
+    /// The bitmap which indicates whether each inode part is used.
+    pub inode_block_bitmap: [u8; 1024 * 1024 / core::mem::size_of::<Inode>()],  // 1024 * 1024 bytes = 1024 blocks, divide by inode size = total inode number
 
     /// The bitmap which indicates whether each inode is used.
-    pub inode_bitmap: [u8; 512],    // 512 * 8 = 4096 = 1 block
+    pub inode_bitmap: [u8; 128],    // 128 * 8 = 1024 = 1 block
 }
 
 impl SuperBlock {
@@ -49,38 +52,49 @@ impl Default for SuperBlock {
     fn default() -> Self {
         Self {
             magic: 0x504B4653,
-            block_size: 4096,
+            block_size: 1024,
             total_blocks: 1024,
             data_start_block: 1,
-            block_bitmap: [0; 512],
-            inode_bitmap: [0; 512],
+            block_bitmap: [0; 128],
+            inode_block_bitmap: [0; 1024 * 1024 / core::mem::size_of::<Inode>()],
+            inode_bitmap: [0; 128],
         }
     }
+}
+
+/// The definition of the file type
+#[derive(Debug, Clone, Copy)]
+pub enum FileType {
+    /// The regular file.
+    Regular = 0,
+
+    /// The directory.
+    Directory = 1,
+
+    /// The device file.
+    Device = 2,
 }
 
 /// The definition of the inode.
 #[repr(C)]
 #[derive(Debug, Clone)]
 pub struct Inode {
+    /// The ID of this inode.
+    pub inode_id: u32,
+
     /// The file type.
     /// 
     /// # Number of this parameter
     /// 0: regular file;
     /// 
     /// 1: directory;
-    pub file_type: u8,
+    pub file_type: FileType,
 
-    /// The permission of the file.
-    pub permission: u8,
+    /// The head block of the file.
+    pub head_block: u32,
 
-    /// The size of the file in bytes.
-    pub size: u64,
-
-    /// The blocks number which the file occupies.
-    pub block_count: u64,
-
-    /// The data of the file, which points to the blocks in the data area.
-    pub data: [u32; 12],
+    /// The file length in bytes.
+    pub file_length: u64,
 }
 
 impl Inode {
@@ -131,7 +145,7 @@ pub struct DirEntry {
     /// The inode number of the directory.
     pub inode: u32,
 
-    /// The name of the directory.
+    /// The name of the directory, which contains up to 255 characters.
     pub name: [u8; 256],
 }
 
--- fs/src/lib.rs
+++ fs/src/lib.rs
@@ -2,6 +2,9 @@
 
 pub mod definition;
 pub mod bitmap;
+pub use bitmap::Bitmap;
+
+use crate::definition::Inode;
 
 /// The block device driver.
 pub trait BlockDevice {
@@ -11,7 +14,7 @@ pub trait BlockDevice {
     /// 
     /// * `block_num` - The block number to read.
     /// * `buf` - The buffer to store the data.
-    fn read_block(&self, block_num: u32, buf: &mut [u8]);
+    fn read_block(&self, block_num: u32, offset: u32, buf: &mut [u8]);
 
     /// Write a block to the block device.
     /// 
@@ -19,7 +22,7 @@ pub trait BlockDevice {
     /// 
     /// * `block_num` - The block number to write.
     /// * `buf` - The data to write.
-    fn write_block(&self, block_num: u32, buf: &[u8]);
+    fn write_block(&self, block_num: u32, offset: u32, buf: &[u8]);
 }
 
 /// The basic structure of the whole file system.
@@ -30,6 +33,9 @@ pub struct FileSystem<B: BlockDevice> {
 
     /// The super block of the file system.
     pub super_block: definition::SuperBlock,
+
+    /// The data start block number.
+    pub data_start_block: u32,
 }
 
 impl<B: BlockDevice> FileSystem<B> {
@@ -44,15 +50,132 @@ impl<B: BlockDevice> FileSystem<B> {
     /// * `Self` - The mounted file system.
     pub fn mount(bd: B) -> Self {
         let mut super_block = definition::SuperBlock::default();
-        bd.read_block(0, &mut super_block.as_mut_bytes());
+        bd.read_block(0, 0, &mut super_block.as_mut_bytes());
         Self {
             block_device: bd,
             super_block: super_block,
+            data_start_block: 1024,  // Will dynamic calculate in the future.
         }
     }
 
     /// Synchronize the file system to the block device.
     pub fn sync(&self) {
-        self.block_device.write_block(0, &self.super_block.as_bytes());
+        self.block_device.write_block(0, 0, &self.super_block.as_bytes());
+    }
+
+
+    /// Get the max inode (which means the file we can store in this fs)
+    /// 
+    /// # Returns
+    /// 
+    /// * `usize` - The max inode number.
+    /// 
+    /// # Note
+    /// 
+    /// If you want to get the max inode number, you should call this method and minus 1.
+    /// 
+    /// # Example
+    /// 
+    /// ```
+    /// let fs = FileSystem::mount(bd);
+    /// let max_inode = fs.get_max_inode();
+    /// let max_inode_id = max_inode - 1;
+    /// ```
+    pub fn get_max_inode(&self) -> usize {
+        ((self.data_start_block - 1) as usize * self.super_block.block_size as usize) / core::mem::size_of::<definition::Inode>()
+    }
+
+    /// Allocate an inode.
+    /// 
+    /// # Returns
+    /// 
+    /// * `(Inode, u32)` - The inode and the block number.
+    fn alloc_inode(&mut self, file_type: definition::FileType) -> Result<(Inode, u32), &'static str> {
+        // Alloc which bitmap has been used.
+        let mut block_bitmap = &mut self.super_block.block_bitmap;
+        let block_num = if let Some(i) = block_bitmap.alloc(128).map(|i| i as u32) {
+            i
+        } else {
+            return Err("No block available");
+        };
+
+        // Alloc which inode has been used.
+        let mut inode_bitmap = &mut self.super_block.inode_bitmap;
+        let inode_num = if let Some(i) = inode_bitmap.alloc(128) {
+            i as u32
+        } else {
+            return Err("No inode available");
+        };
+
+        // Define that inode
+        let inode = Inode {
+            inode_id: inode_num,
+            file_type,
+            head_block: block_num,  // Problem: Can't sure that the behind block is free, being optimized.
+            file_length: 0,
+        };
+        Ok((inode, block_num))
+    }
+
+    /// Create a file.
+    pub fn mkfile(&mut self) {
+        // 1. Allocate an inode.
+        let inode_num = self.alloc_inode(definition::FileType::Regular).unwrap();
+
+        // 2. Write the inode to the block device.
+        let offset = inode_num.0.inode_id as usize * core::mem::size_of::<Inode>();
+        self.block_device.write_block(inode_num.1, offset as u32, &inode_num.0.as_bytes());
+    }
+
+    /// Convert a name to a 256 bytes array.
+    /// 
+    /// # Parameters
+    /// 
+    /// * `name_src` - The name to convert.
+    /// 
+    /// # Returns
+    /// 
+    /// * `[u8; 256]` - The converted name.
+    /// 
+    /// # Example
+    /// 
+    /// ```
+    /// let name = FileSystem::convert_name(b"hello");
+    /// # Exa
+    fn convert_name(name_src: &[u8]) -> [u8; 256] {
+        let mut name = [0u8; 256];
+        let len = name_src.len().min(name.len() - 1);
+        name[..len].copy_from_slice(&name_src[..len]);
+        name
+    }
+
+    /// Create a directory.
+    pub fn mkdir(&mut self) {
+        // 1. Allocate an inode.
+        let inode_num = self.alloc_inode(definition::FileType::Directory).unwrap();
+
+        // 2. Write the inode to the block device.
+        let offset = inode_num.0.inode_id as usize * core::mem::size_of::<Inode>();
+        self.block_device.write_block(inode_num.1, offset as u32, &inode_num.0.as_bytes());
+
+        // 3. Create a '.' and '..' entry in the directory.
+        // 3.1 Create a '.' entry.
+        let name = Self::convert_name(b".");
+        let dot_dir_entry = definition::DirEntry {
+            inode: inode_num.0.inode_id,
+            name,
+        };
+
+        // 3.2 Create a '..' entry.
+        let name = Self::convert_name(b"..");
+        let dot_dot_dir_entry = definition::DirEntry {
+            inode: inode_num.0.inode_id,
+            name,
+        };
+
+        // 3.3 Write the '.' and '..' entry to the block device.
+        let offset = inode_num.0.inode_id as usize * core::mem::size_of::<definition::DirEntry>();
+        self.block_device.write_block(inode_num.1, offset as u32, &dot_dir_entry.as_bytes());
+        self.block_device.write_block(inode_num.1, (offset + core::mem::size_of::<definition::DirEntry>()) as u32, &dot_dot_dir_entry.as_bytes());
     }
 }
