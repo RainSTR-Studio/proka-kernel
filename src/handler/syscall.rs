95bd5e00c223525d42cfcca4c1df831752fa5145
zhangxuan2011 <zx20110412@outlook.com>
2026-07-05 13:21:50 +0800

    fix: :ambulance: Fixed that no `sysretq` after entering syscall

--- src/handler/syscall.rs
+++ src/handler/syscall.rs
@@ -24,7 +24,24 @@ pub extern "C" fn syscall_entry() {
         "push r14",
         "push r15",
         // Enter main function
-        "call syscall_handler"
+        "call syscall_handler",
+        "pop r15",
+        "pop r14",
+        "pop r13",
+        "pop r12",
+        "pop r11",
+        "pop r10",
+        "pop r9",
+        "pop r8",
+        "pop rdi",
+        "pop rsi",
+        "pop rbp",
+        "pop rsp",
+        "pop rdx",
+        "pop rcx",
+        "pop rbx",
+        "pop rax",
+        "sysretq",
     );
 }
 
